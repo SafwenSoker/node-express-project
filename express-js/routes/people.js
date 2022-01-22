c242faf4351885b459fd5c1a1b58b455e33b0f46
safwensoker <safwensoker1@gmail.com>
2022-01-22 18:30:10 +0100

    using router.route

--- express-js/routes/people.js
+++ express-js/routes/people.js
@@ -7,9 +7,12 @@ const {
     deletePerson
 } = require('../controllers/people')
 const router = express.Router()
-router.get('/', getPeople)
-router.post('/', createPerson)
-router.put('/:id', updatePerson)
-router.delete('/:id', deletePerson)
+// router.get('/', getPeople)
+// router.post('/', createPerson)
+// router.put('/:id', updatePerson)
+// router.delete('/:id', deletePerson)
+
+router.route('/').get(getPeople).post(createPerson)
+router.route('/:id').put(updatePerson).delete(deletePerson)
 
 module.exports = router
