9b2970ba1c9e32a640b7b1ad76b976c61c81b5eb
safwensoker <safwensoker1@gmail.com>
2022-01-22 17:17:01 +0100

    third-party middleware : morgan

--- express-js/app-middlewares.js
+++ express-js/app-middlewares.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const app = express()
+const morgan = requrie('morgan')
 const logger = require("./logger-middleware")
 const authorize = require("./authorize")
 // app.use([logger, authorize])
-app.get('/', logger, (req, res) => {
+app.use(morgan('tiny'))
+app.get('/', (req, res) => {
     res.send('homepage')
     console.log(req.user);
 })
-app.get('/about', logger, (req, res) => {
+app.get('/about', (req, res) => {
     res.send('About')
 })
-app.get('/clients', logger, (req, res) => {
+app.get('/clients', (req, res) => {
     res.send('Client profile')
 })
 app.listen(5000)
